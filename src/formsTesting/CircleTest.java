aac210b008a1a5244ba9707c4f753aafd1143857
JonasWolf12 <Jonas.Wolf@hak-reutte.ac.at>
2024-10-08 11:19:00 +0200

    created testToString

--- src/formsTesting/CircleTest.java
+++ src/formsTesting/CircleTest.java
@@ -3,28 +3,28 @@ package formsTesting;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import testNG.MyMath;
+
 
 public class CircleTest {
   @Test
   public void testCalculateCircumference() {
 	  Circle c = new Circle (1,2,5);
 	  double result = c.calculateCircumference();
-	  Assert.assertEquals(result, 50);
+	  Assert.assertEquals(result, 2*5*Math.PI);
   }
   
   @Test
   public void testCalculateArea() {
 	  Circle c = new Circle (1,2,5);
 	  double result = c.calculateArea();
-	  Assert.assertEquals(result, 50);
+	  Assert.assertEquals(result, 5*5*Math.PI);
   }
  
   @Test
   public void testToString() {
-	  Circle c = new Circle (0,0,0);
+	  Circle c = new Circle (1.0,2.0,5.0);
 	  String result = c.toString();
-	  Assert.assertEquals(result, 50);
+	  Assert.assertEquals(result,"Circle (" + 1.0 + "|" + 2.0+ ") with radius = " + 5.0);
   }
   
   
